030d47c7428ecf8f77fbd9abccebd5cf62ca45ab
y.salami <y.salami@asiatech.ir>
2019-01-09 16:36:08 +0330

    bugs fixed in booklist

--- library/frontend/src/components/book/BookList.jsx
+++ library/frontend/src/components/book/BookList.jsx
@@ -51,99 +51,95 @@ class BookList extends Component {
   };
 
   render() {
-    return (
-      (
-        <Paper
-          style={{
-            marginLeft: 120,
-            marginRight: 120,
-            marginTop: 20,
-            padding: 50
-          }}
-        >
-          <Table>
-            <TableHead>
-              <TableRow>
+    return [
+      <Paper
+        style={{
+          marginLeft: 120,
+          marginRight: 120,
+          marginTop: 20,
+          padding: 50
+        }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography variant="title" gutterBottom>
+                  Title
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="title" gutterBottom>
+                  Author
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="title" gutterBottom>
+                  Category
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="title" gutterBottom>
+                  Description
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="caption" gutterBottom>
+                  Delete Book
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.props.books.map((book, id) => (
+              <TableRow key={`book_${id}`}>
+                <TableCell>{book.title}</TableCell>
+                <TableCell>{book.author}</TableCell>
+                <TableCell>{book.category}</TableCell>
+                <TableCell>{book.description}</TableCell>
                 <TableCell>
-                  <Typography variant="title" gutterBottom>
-                    Title
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="title" gutterBottom>
-                    Author
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="title" gutterBottom>
-                    Category
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="title" gutterBottom>
-                    Description
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="caption" gutterBottom>
-                    Delete Book
-                  </Typography>
+                  {book.free === "free" ? (
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      // onClick={() => this.props.deleteBook(id)}
+                      onClick={() => this.handleClickOpen(book)}
+                    >
+                      <DeleteForeverIcon />
+                    </Button>
+                  ) : (
+                    <Button variant="contained" color="secondary" disabled>
+                      <DeleteForeverIcon />
+                    </Button>
+                  )}
                 </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.props.books.map((book, id) => (
-                <TableRow key={`book_${id}`}>
-                  <TableCell>{book.title}</TableCell>
-                  <TableCell>{book.author}</TableCell>
-                  <TableCell>{book.category}</TableCell>
-                  <TableCell>{book.description}</TableCell>
-                  <TableCell>
-                    {book.free === "free" ? (
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        // onClick={() => this.props.deleteBook(id)}
-                        onClick={() => this.handleClickOpen(book)}
-                      >
-                        <DeleteForeverIcon />
-                      </Button>
-                    ) : (
-                      <Button variant="contained" color="secondary" disabled>
-                        <DeleteForeverIcon />
-                      </Button>
-                    )}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Paper>
-      ),
-      (
-        <Dialog
-          open={this.state.open}
-          onClose={this.handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">{"Delete"}</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Are You Sure You Want To Delete?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleDelete} color="primary" autoFocus>
-              Delete
-            </Button>
-          </DialogActions>
-        </Dialog>
-      )
-    );
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>,
+      <Dialog
+        open={this.state.open}
+        onClose={this.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Delete"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are You Sure You Want To Delete?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={this.handleDelete} color="primary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    ];
   }
 }
 
